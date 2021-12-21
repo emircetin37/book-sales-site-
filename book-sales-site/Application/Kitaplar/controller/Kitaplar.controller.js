6a7a4cb0c2e038bc768d3f1c4865cbd691a94b26
emircetin37 <81845901+emircetin37@users.noreply.github.com>
2021-12-21 15:10:25 +0300

    Create Kitaplar.controller.js

---
+++ book-sales-site/Application/Kitaplar/controller/Kitaplar.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+    'com/UI5Starter/Application/Base/BaseController',
+
+], function (BaseController) {
+    'use strict';
+    
+    return BaseController.extend("com.UI5Starter.Application.Kitaplar.controller.Kitaplar", {
+        onInit: function () {
+            
+
+        }
+    })
+});
